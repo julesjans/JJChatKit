6567ae56df1331fb912e9df1f59f83ca8b4f4512
Jules Jans <julesjans@me.com>
2015-07-22 13:14:47 +0100

    Initial Commit

---
+++ JJChatKit/JJChatKit.h
@@ -0,0 +1,19 @@
+//
+//  JJChatKit.h
+//  JJChatKit
+//
+//  Created by Julian Jans on 22/07/2015.
+//  Copyright (c) 2015 Julian Jans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JJChatKit.
+FOUNDATION_EXPORT double JJChatKitVersionNumber;
+
+//! Project version string for JJChatKit.
+FOUNDATION_EXPORT const unsigned char JJChatKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJChatKit/PublicHeader.h>
+
+
